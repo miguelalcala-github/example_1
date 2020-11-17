7d565216b30a1a2685e414829a74a8a2d999bca7
MikeLiam <miguelalcalaromero@gmail.com>
2020-11-17 13:27:45 +0100

    added content-module addapted filter section slider

--- components/content/Carousel.vue
+++ components/content/Carousel.vue
@@ -9,7 +9,7 @@
     </div>
 
     <CarouselCard
-      v-for="(card, index) in currentSectionCards"
+      v-for="(card, index) in cards"
       :key="index"
       :offer="card"
       :class="getClass(currentPageIndex, index)"
@@ -32,135 +32,144 @@ export default {
       default: 'destacados',
     },
   },
+  async fetch() {
+    this.cards = await this.$content('offers')
+      .where({ section: { $contains: this.currentSection } })
+      .fetch()
+  },
   data() {
     return {
-      cards: [
-        {
-          destination: 'Menorca',
-          duration: '8 dias / 7 noches',
-          image:
-            'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/menorca_pbtaru.jpg',
-          discount: '-15%',
-          options: [
-            { type: 'Fly', data: 'Raynair' },
-            { type: 'Host', data: 'Blue Sea San Anton Hotel & Spa', stars: 3 },
-            { type: 'Renting', data: 'Fiat 500 o similiar' },
-          ],
-          price: 339,
-          section: ['baleares y canarias', 'mediterraneo', 'destacados'],
-        },
-        {
-          destination: 'Cancun',
-          duration: '8 dias / 7 noches',
-          image:
-            'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/cancun_krwpsw.jpg',
-          discount: '-30%',
-          special: 'todo incluido',
-          options: [
-            { type: 'Fly', data: 'Regular' },
-            { type: 'Host', data: 'Flamingo Cancun', stars: 4 },
-            { type: 'Transport', data: 'Traslados aeropuerto - hotel' },
-          ],
-          price: 898,
-          section: ['caribe', 'larga distancia', 'destacados'],
-        },
-        {
-          destination: 'Londres',
-          duration: '8 dias / 7 noches',
-          image:
-            'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522468/exercise_1/londres_pbvkav.jpg',
-          discount: '-30%',
-          options: [
-            { type: 'From', data: 'Barcelona' },
-            { type: 'Fly', data: 'Raynair' },
-            { type: 'Renting', data: 'Hotel Lords', stars: 2 },
-          ],
-          price: 150,
-          section: ['capitales europeas', 'destacados'],
-        },
-        {
-          destination: 'Menorca',
-          duration: '8 dias / 7 noches',
-          image:
-            'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/menorca_pbtaru.jpg',
-          discount: '-15%',
-          options: [
-            { type: 'Fly', data: 'Raynair' },
-            { type: 'Host', data: 'Blue Sea San Anton Hotel & Spa', stars: 3 },
-            { type: 'Renting', data: 'Fiat 500 o similiar' },
-          ],
-          price: 339,
-          section: ['baleares y canarias', 'mediterraneo', 'destacados'],
-        },
-        {
-          destination: 'Mallorca',
-          duration: '8 dias / 7 noches',
-          image:
-            'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/menorca_pbtaru.jpg',
-          discount: '-15%',
-          options: [
-            { type: 'Fly', data: 'Raynair' },
-            { type: 'Host', data: 'Blue Sea San Anton Hotel & Spa', stars: 3 },
-            { type: 'Renting', data: 'Fiat 500 o similiar' },
-          ],
-          price: 339,
-          section: ['baleares y canarias', 'mediterraneo', 'destacados'],
-        },
-        {
-          destination: 'Cancun',
-          duration: '8 dias / 7 noches',
-          image:
-            'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/cancun_krwpsw.jpg',
-          discount: '-30%',
-          special: 'todo incluido',
-          options: [
-            { type: 'Fly', data: 'Regular' },
-            { type: 'Host', data: 'Flamingo Cancun', stars: 4 },
-            { type: 'Transport', data: 'Traslados aeropuerto - hotel' },
-          ],
-          price: 898,
-          section: ['caribe', 'larga distancia', 'destacados'],
-        },
-        {
-          destination: 'Londres',
-          duration: '8 dias / 7 noches',
-          image:
-            'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522468/exercise_1/londres_pbvkav.jpg',
-          discount: '-30%',
-          options: [
-            { type: 'From', data: 'Barcelona' },
-            { type: 'Fly', data: 'Raynair' },
-            { type: 'Renting', data: 'Hotel Lords', stars: 2 },
-          ],
-          price: 150,
-          section: ['capitales europeas', 'destacados'],
-        },
-        {
-          destination: 'Mallorca',
-          duration: '8 dias / 7 noches',
-          image:
-            'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/menorca_pbtaru.jpg',
-          discount: '-15%',
-          options: [
-            { type: 'Fly', data: 'Raynair' },
-            { type: 'Host', data: 'Blue Sea San Anton Hotel & Spa', stars: 3 },
-            { type: 'Renting', data: 'Fiat 500 o similiar' },
-          ],
-          price: 339,
-          section: ['baleares y canarias', 'mediterraneo', 'destacados'],
-        },
-      ],
+      cards: [],
+      // cards2: [
+      //   {
+      //     destination: 'Menorca',
+      //     duration: '8 dias / 7 noches',
+      //     image:
+      //       'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/menorca_pbtaru.jpg',
+      //     discount: '-15%',
+      //     options: [
+      //       { type: 'Fly', data: 'Raynair' },
+      //       { type: 'Host', data: 'Blue Sea San Anton Hotel & Spa', stars: 3 },
+      //       { type: 'Renting', data: 'Fiat 500 o similiar' },
+      //     ],
+      //     price: 339,
+      //     section: ['baleares y canarias', 'mediterraneo', 'destacados'],
+      //   },
+      //   {
+      //     destination: 'Cancun',
+      //     duration: '8 dias / 7 noches',
+      //     image:
+      //       'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/cancun_krwpsw.jpg',
+      //     discount: '-30%',
+      //     special: 'todo incluido',
+      //     options: [
+      //       { type: 'Fly', data: 'Regular' },
+      //       { type: 'Host', data: 'Flamingo Cancun', stars: 4 },
+      //       { type: 'Transport', data: 'Traslados aeropuerto - hotel' },
+      //     ],
+      //     price: 898,
+      //     section: ['caribe', 'larga distancia', 'destacados'],
+      //   },
+      //   {
+      //     destination: 'Londres',
+      //     duration: '8 dias / 7 noches',
+      //     image:
+      //       'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522468/exercise_1/londres_pbvkav.jpg',
+      //     discount: '-30%',
+      //     options: [
+      //       { type: 'From', data: 'Barcelona' },
+      //       { type: 'Fly', data: 'Raynair' },
+      //       { type: 'Renting', data: 'Hotel Lords', stars: 2 },
+      //     ],
+      //     price: 150,
+      //     section: ['capitales europeas', 'destacados'],
+      //   },
+      //   {
+      //     destination: 'Menorca',
+      //     duration: '8 dias / 7 noches',
+      //     image:
+      //       'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/menorca_pbtaru.jpg',
+      //     discount: '-15%',
+      //     options: [
+      //       { type: 'Fly', data: 'Raynair' },
+      //       { type: 'Host', data: 'Blue Sea San Anton Hotel & Spa', stars: 3 },
+      //       { type: 'Renting', data: 'Fiat 500 o similiar' },
+      //     ],
+      //     price: 339,
+      //     section: ['baleares y canarias', 'mediterraneo', 'destacados'],
+      //   },
+      //   {
+      //     destination: 'Mallorca',
+      //     duration: '8 dias / 7 noches',
+      //     image:
+      //       'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/menorca_pbtaru.jpg',
+      //     discount: '-15%',
+      //     options: [
+      //       { type: 'Fly', data: 'Raynair' },
+      //       { type: 'Host', data: 'Blue Sea San Anton Hotel & Spa', stars: 3 },
+      //       { type: 'Renting', data: 'Fiat 500 o similiar' },
+      //     ],
+      //     price: 339,
+      //     section: ['baleares y canarias', 'mediterraneo', 'destacados'],
+      //   },
+      //   {
+      //     destination: 'Cancun',
+      //     duration: '8 dias / 7 noches',
+      //     image:
+      //       'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/cancun_krwpsw.jpg',
+      //     discount: '-30%',
+      //     special: 'todo incluido',
+      //     options: [
+      //       { type: 'Fly', data: 'Regular' },
+      //       { type: 'Host', data: 'Flamingo Cancun', stars: 4 },
+      //       { type: 'Transport', data: 'Traslados aeropuerto - hotel' },
+      //     ],
+      //     price: 898,
+      //     section: ['caribe', 'larga distancia', 'destacados'],
+      //   },
+      //   {
+      //     destination: 'Londres',
+      //     duration: '8 dias / 7 noches',
+      //     image:
+      //       'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522468/exercise_1/londres_pbvkav.jpg',
+      //     discount: '-30%',
+      //     options: [
+      //       { type: 'From', data: 'Barcelona' },
+      //       { type: 'Fly', data: 'Raynair' },
+      //       { type: 'Renting', data: 'Hotel Lords', stars: 2 },
+      //     ],
+      //     price: 150,
+      //     section: ['capitales europeas', 'destacados'],
+      //   },
+      //   {
+      //     destination: 'Mallorca',
+      //     duration: '8 dias / 7 noches',
+      //     image:
+      //       'https://res.cloudinary.com/da3z5stec/image/upload/h_720,w_1280/v1605522469/exercise_1/menorca_pbtaru.jpg',
+      //     discount: '-15%',
+      //     options: [
+      //       { type: 'Fly', data: 'Raynair' },
+      //       { type: 'Host', data: 'Blue Sea San Anton Hotel & Spa', stars: 3 },
+      //       { type: 'Renting', data: 'Fiat 500 o similiar' },
+      //     ],
+      //     price: 339,
+      //     section: ['baleares y canarias', 'mediterraneo', 'destacados'],
+      //   },
+      // ],
       currentPageIndex: 0,
       cardsPerPage: 4,
       currentCards: [],
     }
   },
-  computed: {
-    currentSectionCards() {
-      this.generateCurrentSectionCards()
-      return this.currentCards
-    },
+  watch: {
+    currentSection: '$fetch',
   },
+  // computed: {
+  //   currentSectionCards() {
+  //     this.generateCurrentSectionCards()
+  //     return this.currentCards
+  //   },
+  // },
   methods: {
     getClass(pageIndex, index) {
       const minIndex = this.cardsPerPage * pageIndex
@@ -169,15 +178,15 @@ export default {
       return { cardShow: !(index >= minIndex && index < maxIndex) }
     },
     goTo(index) {
-      if (index >= 0 && this.cardsPerPage * index < this.currentCards.length) {
+      if (index >= 0 && this.cardsPerPage * index < this.cards.length) {
         this.currentPageIndex = index
       }
     },
-    generateCurrentSectionCards() {
-      this.currentCards = this.cards.filter((card) =>
-        card.section.some((section) => section.includes(this.currentSection))
-      )
-    },
+    // generateCurrentSectionCards() {
+    //   this.currentCards = this.cards.filter((card) =>
+    //     card.section.some((section) => section.includes(this.currentSection))
+    //   )
+    // },
   },
 }
 </script>
